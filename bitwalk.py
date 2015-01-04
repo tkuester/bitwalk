d5d3a1b4398b907c39a1688031088c3d9f00d1d2
Tim Kuester <tkuester@users.noreply.github.com>
2015-01-04 12:27:43 -0500

    Reorganizing and cleaning up code

--- bitwalk.py
+++ bitwalk.py
@@ -3,58 +3,80 @@ import sys
 import optparse
 import curses
 
-from bwgui import BitWalkGui
+from bwbuffer import BitWalkBuffer
 
 class BitWalk(object):
     def __init__(self, vals, args):
         self.vals = vals
         self.args = args
 
-        self.stdscr = None
-        self.running = True
+        self.running = False
+        self.buffers = []
+        self.active_buffer = 0
 
     def do_cmd(self, cmd):
         if cmd == 'q':
             self.running = False
 
+    def clear_status(self):
+        self.stdscr.move(self.max_y - 1, 0)
+        self.stdscr.clrtoeol()
+
+    def status_msg(self, message):
+        self.clear_status()
+        self.stdscr.addstr(self.max_y - 1, 0, message)
+
+    def status_query(self, query):
+        self.status_msg(query)
+
+        curses.echo()
+        ret = self.stdscr.getstr()
+        curses.noecho()
+
+        return ret.strip()
+
+    def resize(self):
+        (self.max_y, self.max_x) = self.stdscr.getmaxyx()
+        curses.resizeterm(self.max_y, self.max_x)
+
+    def get_active_buffer(self):
+        return self.buffers[self.active_buffer]
+
     def run(self, stdscr):
-        bwg = BitWalkGui(self, stdscr)
+        # Setup Curses
+        self.stdscr = stdscr
 
-        for i, path in enumerate(self.args):
-            if i == 0:
-                bwg.buffers[0].open(path)
-            else:
-                bwg.new_buffer(path)
+        curses.noecho()
+        if curses.has_colors():
+            curses.start_color()
 
+        self.resize()
+
+        win = curses.newwin(10, 10, 0, 0)
+        win.border()
+
+        self.running = True
         while self.running:
-            c = stdscr.getch()
-            bwg.clear_status()
+            c = self.stdscr.getch()
 
             if c == curses.KEY_RESIZE:
-                bwg.resize()
-            elif c == ord(':'):
-                cmd = bwg.status_query(':')
+                self.resize()
+                continue
+
+            self.clear_status()
+
+            if c == ord(':'):
+                cmd = self.status_query(':')
                 self.do_cmd(cmd)
-            elif c == curses.KEY_UP:
-                bwg.up()
-            elif c == curses.KEY_DOWN:
-                bwg.down()
-            elif c == curses.KEY_LEFT:
-                bwg.left()
-            elif c == curses.KEY_RIGHT:
-                bwg.right()
-            elif c == ord('/'):
-                search = bwg.status_query('/')
-                bwg.get_active_buffer().search(search)
-            elif c == ord('n'):
-                bwg.get_active_buffer().search()
-            elif c == ord('N'):
-                bwg.status_msg('TODO')
 
 def main():
-    usage = 'usage: %prog [options] [file ..]'
+    usage = 'usage: %prog [options] file [...]'
     op = optparse.OptionParser(usage)
     (vals, args) = op.parse_args()
+
+    if len(args) == 0:
+        print 'No files to read'
+        return
 
     bw = BitWalk(vals, args)
     curses.wrapper(bw.run)
